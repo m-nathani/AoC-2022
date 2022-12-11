5474914573156aba42e75b1223470b0182533b02
Murtaza <mudza100@gmail.com>
2022-12-12 07:03:55 +0800

    day 3 answer

---
+++ src/days/day-3.ts
@@ -0,0 +1,61 @@
+import { readFileFromDay } from "../utils/index";
+
+// Lowercase item types a through z have priorities 1 through 26.
+// Uppercase item types A through Z have priorities 27 through 52.
+// Getting the priority by subtracting the asci number to get 1 - 52 output
+const getPriority = (char: string) => {
+  const charCode: number = char.charCodeAt(0);
+  if (charCode >= 65 && charCode <= 90) {
+    return charCode - 38;
+  } else if (charCode >= 97 && charCode <= 122) {
+    return charCode - 96;
+  }
+  return 0;
+};
+
+const part1 = (data: string[]) => {
+  let rucksackPriority: number = 0;
+
+  data.forEach((rucksack: string) => {
+    const rucksackHalfCount = rucksack.length / 2;
+    const rucksackFirstHalf = rucksack.slice(0, rucksackHalfCount);
+    const rucksackSecondHalf = rucksack.slice(rucksackHalfCount);
+    // using Set to remove repeating item in half
+    new Set(rucksackFirstHalf).forEach((item: string) => {
+      if (rucksackSecondHalf.includes(item)) {
+        rucksackPriority += getPriority(item);
+      }
+    });
+  });
+
+  return `Rucksack Priority: ${rucksackPriority}`;
+};
+
+const part2 = (data: string[]) => {
+  let rucksackPriority: number = 0;
+  const groupOfRucksack = 3;
+
+  for (let i = 0; i < data.length; i = i + groupOfRucksack) {
+    // using Set to remove repeating item in half
+    new Set(data[i]).forEach((item: string) => {
+      if (data[i + 1].includes(item) && data[i + 2].includes(item)) {
+        rucksackPriority += getPriority(item);
+      }
+    });
+  }
+
+  return `Rucksack Priority in group: ${rucksackPriority}`;
+};
+
+export default (day: string) => {
+  try {
+    const input: string = readFileFromDay(day);
+    const data: string[] = input.split("\n");
+    return {
+      part1: part1(data),
+      part2: part2(data),
+    };
+  } catch (error) {
+    console.error(`Error on day : ${day}`, error);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
-import day1 from "./days/day-1";
-import day2 from "./days/day-2";
 import { prompt } from "./utils";
 
-prompt("Please enter the day you want to run advent of code :", (day) => {
+prompt("Please enter the day you want to run advent of code :", async (day) => {
   try {
-    let answer: { part1?: string; part2?: string } | undefined = {};
-
-    switch (day) {
-      case "1":
-        answer = day1(day);
-        break;
-      case "2":
-        answer = day2(day);
-        break;
-      default:
-        throw "Invalid day entered";
-    }
+    const adventDay = await import(`./days/day-${day}`);
+    const answer: { part1?: string; part2?: string } | undefined = adventDay.default(day);
 
     console.table([{ Day: 1, Part1: answer?.part1, Part2: answer?.part2 }]);
   } catch (error) {
-    console.error(`No AdventOfCode exists for the day: ${day}`);
+    console.error(`No AdventOfCode exists for the day: ${day}`, error);
   }
 });
